874a118e5eb5a2fa1a6f930450162c9fb7d9a8b7
hassanhamdani <96621474+hassanhamdani@users.noreply.github.com>
2023-03-31 18:42:11 +0400

    Uploading Training files

---
+++ Assets/visbilityTogger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visbilityToggler : MonoBehaviour
+
+{
+    // private MeshRenderer rend;
+    // public void toggleVisibility()
+    //
+
+    //rend = GetComponent<MeshRenderer>();
+    //bool visibility = rend.enable;
+    //rend.enable = !visibility;
+    //}
+
+}
